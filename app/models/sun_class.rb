53d6c0ac2a601dfdd8b9bf266fd5a6eb4bbfdedb
scratchit <morsislam@gmail.com>
2013-09-30 15:27:09 -0700

    typo fix

--- app/models/sun_class.rb
+++ app/models/sun_class.rb
@@ -1,7 +1,7 @@
 class SunClass < ActiveRecord::Base
   extend Importable
-  has_many :class_alignments, :dependent => :destroy
-  has_many :students, :through => :class_alignments
+  has_many :class_assignments, :dependent => :destroy
+  has_many :students, :through => :class_assignments
 
   has_many :wait_list_assignments, :dependent => :destroy
   has_many :wait_list_students, :class_name => Student, :through => :wait_list_assignments
